a43863b84991be61a5370a2f39fa6499eaede95d
Futoro <realityspectrumlive@gmail.com>
2024-09-15 18:49:50 -0400

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Smart FAQs'}</span>
           </a>
         </span>
       </Show>
